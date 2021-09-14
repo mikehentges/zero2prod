87d07af5efac4fecaaebbaa940070ac111f465ca
Michael Hentges <5008447+mikehentges@users.noreply.github.com>
2021-09-13 20:33:49 -0500

    transition to desktop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-    println!("A second line");
-    println!("A third line");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
