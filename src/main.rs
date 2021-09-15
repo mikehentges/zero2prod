5db6f631193542650154d69636f71edc0418592d
Michael Hentges <mike@mhentges.com>
2021-09-14 19:49:17 -0500

    end of free section

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use zero2prod::run;
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
+    run(listener)?.await
 }
