27414972189d12399b8e9eec3d43d8e58058e304
Michael Hentges <5008447+mikehentges@users.noreply.github.com>
2021-10-13 19:34:29 -0500

    made status mandatory

---
+++ migrations/20211014003208_make_status_not_null_in_subscriptions.sql.sql
@@ -0,0 +1,11 @@
+-- Add migration script here
+BEGIN;
+-- Backfill `status` for historical entries
+UPDATE subscriptions
+SET status = 'confirmed'
+WHERE status IS NULL;
+-- Make `status` mandatory
+ALTER TABLE subscriptions
+ALTER COLUMN status
+SET NOT NULL;
+COMMIT;
