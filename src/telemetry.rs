8a1b6d3fc538e375270c5965d208b0b9f5360966
Michael Hentges <5008447+mikehentges@users.noreply.github.com>
2021-10-18 10:24:00 -0500

    ready for outside review

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -5,63 +5,21 @@ use tracing_log::LogTracer;
 use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-use tonic::metadata::{MetadataKey, MetadataMap};
-use url::Url;
+//use tonic::metadata::{MetadataKey, MetadataMap};
+//use url::Url;
 
 use opentelemetry::sdk::trace::IdGenerator;
 use opentelemetry::sdk::{trace, Resource};
-use opentelemetry::trace::FutureExt;
+//use opentelemetry::trace::FutureExt;
 use opentelemetry::KeyValue;
 use opentelemetry_otlp::WithExportConfig;
-use std::env::{remove_var, var};
-use std::{env::vars, str::FromStr};
-use tracing_opentelemetry::OpenTelemetryLayer;
+//use std::env::{remove_var, var};
+//use std::{env::vars, str::FromStr};
+//use tracing_opentelemetry::OpenTelemetryLayer;
 
-const ENDPOINT: &str = "OLTP_TONIC_ENDPOINT";
-const HEADER_PREFIX: &str = "OLTP_TONIC_";
-// $Env:OLTP_TONIC_ENDPOINT = "https://api.honeycomb.io:443" = old endpoint
+//const ENDPOINT: &str = "OLTP_TONIC_ENDPOINT";
+//const HEADER_PREFIX: &str = "OLTP_TONIC_";
 
-// fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
-//     let endpoint = var(ENDPOINT).unwrap_or_else(|_| {
-//         panic!(
-//             "You must specify and endpoint to connect to with the variable {:?}.",
-//             ENDPOINT
-//         )
-//     });
-//     let endpoint = Url::parse(&endpoint).expect("endpoint is not a valid url");
-//     remove_var(ENDPOINT);
-//     let mut metadata = MetadataMap::new();
-//     for (key, value) in vars()
-//         .filter(|(name, _)| name.starts_with(HEADER_PREFIX))
-//         .map(|(name, value)| {
-//             let header_name = name
-//                 .strip_prefix(HEADER_PREFIX)
-//                 .map(|h| h.replace("_", "-"))
-//                 .map(|h| h.to_ascii_lowercase())
-//                 .unwrap();
-//             (header_name, value)
-//         })
-//     {
-//         metadata.insert(MetadataKey::from_str(&key).unwrap(), value.parse().unwrap());
-//     }
-//
-//     opentelemetry_otlp::new_pipeline()
-//         .tracing()
-//         .with_exporter(
-//             opentelemetry_otlp::new_exporter()
-//                 .tonic()
-//                 .with_endpoint(endpoint.as_str())
-//                 .with_metadata(dbg!(metadata))
-//                 .with_tls_config(
-//                     ClientTlsConfig::new().domain_name(
-//                         endpoint
-//                             .host_str()
-//                             .expect("the specified endpoint should have a valid host"),
-//                     ),
-//                 ),
-//         )
-//         .install_batch(opentelemetry::runtime::Tokio)
-// }
 pub fn get_subscriber(
     name: String,
     env_filter: String,
